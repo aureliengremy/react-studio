dbf37e7a44c326b785b4361a3f827c83202a52f0
Aurelien Gremy <gremy.aurelien@gmail.com>
2022-10-05 17:00:50 -0400

    Add component with basic config to render the mesh/scene - add the model with texture

---
+++ src/Scene.jsx
@@ -0,0 +1,77 @@
+import { Fragment } from "react";
+import Studio from './Studio';
+import {
+    CubeCamera,
+    Environment,
+    OrbitControls,
+    PerspectiveCamera,
+  } from "@react-three/drei";
+import {
+    EffectComposer,
+    DepthOfField,
+    Bloom,
+    ChromaticAberration,
+  } from "@react-three/postprocessing";
+import { BlendFunction } from "postprocessing";
+
+
+const Scene = () => {
+  return (
+    <Fragment>
+      <OrbitControls target={[0, 1.4, 0]} maxPolarAngle={1.45} />
+      <PerspectiveCamera makeDefault fov={50} position={[0, 2, 6]} />
+      <color args={[0, 0, 0]} attach="background" />
+
+      {/* make all the element reflect all light texture */}
+      {/* <CubeCamera resolution={256} frames={Infinity}>
+        {(texture) => (
+          <Fragment>
+            <Environment map={texture} />
+            <Guitar />
+          </Fragment>
+        )}
+      </CubeCamera> */}
+      <Studio/>
+      
+      <spotLight
+        color={[1, 0.25, 0.7]}
+        intensity={1.5}
+        angle={0.6}
+        penumbra={0.5}
+        position={[5, 5, 0]}
+        castShadow
+        shadow-bias={-0.0001}
+      />
+      <spotLight
+        color={[0.14, 0.5, 1]}
+        intensity={2}
+        angle={0.6}
+        penumbra={0.5}
+        position={[-5, 5, 0]}
+        castShadow
+        shadow-bias={-0.0001}
+      />
+
+      {/* <EffectComposer> */}
+        {/* Make the scene and models more blur when you move back with the camera */}
+        {/* <DepthOfField focusDistance={0.0035} focalLength={0.01} bokehScale={3} height={480} /> */}
+        {/* <Bloom
+          blendFunction={BlendFunction.ADD}
+          intensity={2.3} // The bloom intensity.
+          width={300} // render width
+          height={300} // render height
+          kernelSize={5} // blur kernel size
+          // luminanceThreshold={0.8} // luminance threshold. Raise this value to mask out darker elements in the scene.
+          // luminanceSmoothing={0.025} // smoothness of the luminance threshold. Range is [0, 1]
+        /> */}
+        {/* <ChromaticAberration
+          blendFunction={BlendFunction.NORMAL} // blend mode
+          offset={[0.0006, 0.0006]} // color offset
+        /> */}
+      {/* </EffectComposer> */}
+
+    </Fragment>
+  )
+}
+
+export default Scene
---
+++ src/Studio.jsx
@@ -0,0 +1,34 @@
+import { useFrame, useLoader } from '@react-three/fiber'
+import React, { useEffect } from 'react'
+import { Mesh } from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+
+
+const Guitar = () => {
+    const gltf = useLoader(
+        GLTFLoader, "/scene/proberaum_-_rehearsal_room/scene.gltf"
+    )
+
+    useEffect(()=> {
+        gltf.scene.scale.set(1.5, 1.5, 1.5);
+        gltf.scene.position.set(0, 1.8, 0);
+        gltf.scene.traverse((object)=> {
+            if(object instanceof Mesh) {
+                object.castShadow = true;
+                object.receiveShadow = true;
+                object.material.envMapIntensity = 20;
+            }
+        });
+    }, [gltf])
+
+    useFrame((state, delta) => {
+        // gltf.scene.rotation.set()
+        gltf.scene.rotation.x += delta * 0.1;
+        gltf.scene.rotation.y += delta * 0.3;
+    })
+  return (
+    <primitive object={gltf.scene}/>
+  )
+}
+
+export default Guitar
