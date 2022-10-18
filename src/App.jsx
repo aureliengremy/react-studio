510281610711b7921ac60cddd9c5f65b6b48cc8c
Aurelien Gremy <gremy.aurelien@gmail.com>
2022-10-17 20:38:50 -0400

    add Tailwind + config and init - add list of iframe songs from soundcloud

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,47 @@
 import { Fragment, Suspense, useState } from "react";
 import "./App.css";
 import { Canvas } from "@react-three/fiber";
-import Scene  from "./Scene";
-
-
+import Scene from "./Scene";
+import EmbediFrame from "./components/EmbediFrame";
 
 const App = () => {
   return (
-    <div className="container-scene">
-      <div className="music">
-      {/* <iframe width="100%" height="166" scrolling="no" frameBorder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/15833511&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe> */}
-
+    <Fragment>
+      <div className="container-music">
+        <div className="absolute z-10 w-full h-full flex flex-row justify-around items-center">
+          <div className="w-1/2 m-6 lg:mb-0 h-5/6">
+            <div className="relative flex flex-col bg-white/50 rounded-lg shadow-lg h-full overflow-auto">
+              
+                <EmbediFrame />
+                <EmbediFrame />
+                <EmbediFrame />
+                <EmbediFrame />
+                <EmbediFrame />
+                <EmbediFrame />
+                <EmbediFrame />
+                <EmbediFrame />
+              
+            </div>
+          </div>
+          <div className="w-1/2 m-6 lg:mb-0">
+            <div className="relative block bg-white rounded-lg shadow-lg">
+              <div className="p-6">
+                <p>block 2</p>
+                {/* <EmbediFrame /> */}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <Suspense fallback={null}>
-        <Canvas shadows>
-          <Scene />
-        </Canvas>
-      </Suspense>
-    </div>
+      <div className="container-scene">
+        <Suspense fallback={null}>
+          <Canvas shadows>
+            <Scene />
+          </Canvas>
+        </Suspense>
+      </div>
+    </Fragment>
   );
-}
+};
 
 export default App;
---
+++ src/components/EmbediFrame.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react'
+
+const EmbediFrame = () => {
+    // useEffect(() => {
+    //     const img = document.getElementsByClassName('compactSound__artwork')
+    //     console.log(img);
+    // }, [])
+    
+  return (
+    <div className="m-2">
+        <iframe width="100%" height="100" scrolling="no" frameBorder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1364853535&color=%23ff5500&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_reposts=false&show_teaser=false"></iframe>
+    </div>
+  )
+}
+
+export default EmbediFrame
